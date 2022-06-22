f98a3c46534c6d1e9e06ae190595def22c6420a6
Osuohia Emmanuel <josephe442@gmail.com>
2022-06-22 15:31:14 +0100

    added frontend folder setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDb = async () => {
     // creating the connection to the database
     const conn = await mongoose.connect(process.env.MONGO_URI)
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
-  } catch {
+  } catch (error) {
     console.log(`Error: ${error.message}`.red.underline.bold)
     process.exit(1)
   }
