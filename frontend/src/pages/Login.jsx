8d80dae24f49368f889bfd58b9ae89d4e67c4aa9
Osuohia Emmanuel <josephe442@gmail.com>
2022-08-31 16:27:07 +0100

    change text

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,60 +1,60 @@
-import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify'
-import { FaSignInAlt } from 'react-icons/fa'
-import { useSelector, useDispatch } from 'react-redux'
-import { login, reset } from '../features/auth/authSlice'
-import Spinner from '../components/atoms/Spinner'
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { FaSignInAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { login, reset } from "../features/auth/authSlice";
+import Spinner from "../components/atoms/Spinner";
 
 function Login() {
   const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  })
+    email: "",
+    password: "",
+  });
 
   // Destructuring formData
-  const { email, password } = formData
+  const { email, password } = formData;
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const { user, isLoading, isSuccess, isError, message } = useSelector(
     (state) => state.auth
-  )
+  );
 
   // Handing for onChange event
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
-    })
-  }
+    });
+  };
 
   // Handing for onSubmit event
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const userData = {
       email,
       password,
-    }
-    dispatch(login(userData))
-  }
+    };
+    dispatch(login(userData));
+  };
 
   useEffect(() => {
     if (isError) {
-      toast.error(message)
+      toast.error(message);
     }
 
     // Redirect when logged in
     if (isSuccess || user) {
-      navigate('/')
+      navigate("/");
     }
-    dispatch(reset())
-  }, [dispatch, isError, isSuccess, message, navigate, user])
+    dispatch(reset());
+  }, [dispatch, isError, isSuccess, message, navigate, user]);
 
   if (isLoading) {
-    return <Spinner />
+    return <Spinner />;
   }
 
   return (
@@ -63,7 +63,7 @@ function Login() {
         <h1>
           <FaSignInAlt /> Login
         </h1>
-        <p>Please login to get support</p>
+        <p>Please login to get supports</p>
       </section>
       <section className="form">
         <form onSubmit={handleSubmit}>
@@ -96,7 +96,7 @@ function Login() {
         </form>
       </section>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
